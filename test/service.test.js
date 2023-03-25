58fc01c38dff2225a73fdfd9a2040b8dd4fcb297
thuongph <phamhuyenthuong290101@gmail.com>
2023-03-26 05:55:04 +0700

    test: test unit test

--- test/service.test.js
+++ test/service.test.js
@@ -21,7 +21,7 @@ describe('should test Service', () => {
     };
     const _responseData = {
       data: {
-        field: '_field',
+        field: '__field',
       },
     };
     axios.get.mockImplementationOnce(() => Promise.resolve(responseData));
