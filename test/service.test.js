d7072c08c6bfe6fa3704e65358007de36d932346
thuongph <phamhuyenthuong290101@gmail.com>
2023-05-09 18:35:28 +0700

    test: test pipeline success without unit tests stage

--- test/service.test.js
+++ test/service.test.js
@@ -21,7 +21,7 @@ describe('should test Service', () => {
     };
     const demoResponseData = {
       data: {
-        field: 'another test field 10.05.23',
+        field: 'and another test field 10.05.23',
       },
     };
     axios.get.mockImplementationOnce(() => Promise.resolve(responseData));
