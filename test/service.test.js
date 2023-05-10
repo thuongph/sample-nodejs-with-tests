bccf516daa1a3e406519f8140bcd80e69be4ebda
thuongph <phamhuyenthuong290101@gmail.com>
2023-05-10 09:26:57 +0700

    demo: demo success pipeline 10.05

--- test/service.test.js
+++ test/service.test.js
@@ -21,7 +21,7 @@ describe('should test Service', () => {
     };
     const demoResponseData = {
       data: {
-        field: '__field',
+        field: 'field',
       },
     };
     axios.get.mockImplementationOnce(() => Promise.resolve(responseData));
